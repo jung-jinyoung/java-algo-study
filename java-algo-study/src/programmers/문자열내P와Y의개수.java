a3bad9616afb15a172d271b5df33035fc4ae6c59
jinyoung jung <ya_1001@naver.com>
2026-05-09 00:32:45 +0900

    feat:pgm 문자열 기본 연습

--- java-algo-study/src/programmers/문자열내P와Y의개수.java
+++ java-algo-study/src/programmers/문자열내P와Y의개수.java
@@ -1,4 +1,30 @@
 package programmers;
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java
+ * 문자열 연습
+ * 배열 전환 후 filter문도 가능 할 것 같다.
+ */
+
+// 문자열 인덱스로 하나씩 접근 toCharAt()
 public class 문자열내P와Y의개수 {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count = 0 ; // p -> +1, y -> -1
+        System.out.println(s.chars());
+
+        s = s.toLowerCase(); // 모두 소문자로 변환
+        for (int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if (c == 'p'){
+                count++;
+            } else if (c == 'y'){
+                count--;
+            } else {
+                continue;
+            }
+        };
+
+        return count == 0 ;
+    }
 }
--- java-algo-study/src/programmers/문자열정렬하기.java
+++ java-algo-study/src/programmers/문자열정렬하기.java
@@ -1,4 +1,28 @@
 package programmers;
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120850
+ * 문자열 + 배열리스트 연습
+ */
+
+
+import java.util.*;
 
 public class 문자열정렬하기 {
+    public int[] solution(String my_string) {
+        // 1. 숫자를 담을 가변 리스트 생성
+        ArrayList<Integer> check = new ArrayList<>();
+        for (int i = 0 ; i < my_string.length() ; i++){
+            char c = my_string.charAt(i);
+            if (Character.isDigit(c)){
+                check.add(Character.getNumericValue(c));
+            }
+        }
+        Collections.sort(check); // 오름차순 정렬
+        // 정답 배열 초기화
+        int [] answer = new int[check.size()];
+        for (int j = 0 ; j < check.size(); j++){
+            answer[j] = check.get(j);
+        }
+        return answer;
+    }
 }
